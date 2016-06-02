6ef78b3c4a1588aec7e08373f2b6fa18b9bc5b6a
ies <ies>
2016-06-02 17:14:57 +0200

    Refactoring del codi del penjat

--- JavaDoc/src/Main.java
+++ JavaDoc/src/Main.java
@@ -7,16 +7,14 @@ import java.util.Random;
  * Té com atributs tres atributs statics i protected.
  * paraules: Vector de String que ens guarde tres paraules diferents.
  * index: Enter que ens guarda la posició al vector.
- * p: String que ens guarda les lletres introduirdes per l'usuari.
+ * paraula_usuari: String que ens guarda les lletres introduides per l'usuari.
  *  
  * @author Joan Hombrado
  * 
  */
 public class Main {
 
-    static protected String paraules[] = {"hola","programació","casa"};
-    static protected int index;
-    static protected String p;
+    
     /**
      * Mètode principal del programa, es on hem creat tot el joc, printem el "tauler" del joc i l'usuari realment juga
      * per tant es el metode on es desenvolupa la partida.
@@ -31,26 +29,9 @@ public class Main {
      * @param args Vector d'String que rep el mètode on són els arguments que fa servir el main.
      */
     public static void main(String[] args) {
-        System.out.println("Comença el joc!");
-
-        Random rand = new Random();
-        index = rand.nextInt(3);
-        for (int i=0; i<paraules[index].length(); i++) {
-            p += "_";
-        }
-
-        System.out.println(p);
-
-        Scanner reader = new Scanner(System.in);
-
-        int num_errors = 0;
-
-        while (!p.equals(paraules[index]) && num_errors < 5) {
-            if (!nova_lletra(reader.next().charAt(0))) {
-                num_errors++;
-            }
-            System.out.println(p);
-        }
+    	Partida P1 = new Partida();
+    	P1.Jugar();
+        
    }
     
     /**
@@ -62,21 +43,7 @@ public class Main {
      * @param nova char que es rep com a parametre, conté la lletra que ha introduit l'usuari.
      * @return trobada variable bool que ens diu si la lletra que ens ha introduit l'usuari hi es o no al String p.
      */
-    static public boolean nova_lletra(char nova) {
-        boolean trobada = false;
-        String paraula_old = p;
-        p = "";
-        for (int i=0; i<paraules[index].length(); i++) {
-            if (paraules[index].charAt(i) == nova) {
-                p += paraules[index].charAt(i);
-                trobada = true;
-            }
-            else {
-                p += paraula_old.charAt(i);
-            }
-        }
-        return trobada;
-    }
+       
 
 }
 
---
+++ JavaDoc/src/Partida.java
@@ -0,0 +1,64 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Partida {
+	static protected String paraules[] = {"hola","programació","casa"};
+    static protected int index;
+    static protected String paraula_usuari;
+	
+	public Partida(){
+		Random rand = new Random();
+	    index = rand.nextInt(3);
+	}
+	
+	
+	public void Jugar(){
+		
+		System.out.println("Comença el joc!");
+
+	    
+	    
+	    ///CONSTANT QUE CONTE LA LLARGADA DE LA PARAULA
+	    final int largada_paraula = paraules[index].length();
+	    
+		for (int i=0; i<largada_paraula+1; i++) {
+	        paraula_usuari += "_";
+	    }
+
+	    System.out.println(paraula_usuari);
+
+	    Scanner reader = new Scanner(System.in);
+
+	    int num_errors = 0;
+
+	    while (!paraula_usuari.equals(paraules[index]) && num_errors < 5) {
+	    	///VARIABLE QUE ENS GUARDA SI LA LLETRA ES TROBADA O NO.
+	        boolean lletra_trobada = nova_lletra(reader.next().charAt(0));
+	        
+			if (!lletra_trobada) {
+	            num_errors++;
+	        }
+	        System.out.println(paraula_usuari);
+	    }
+
+	}
+	
+	public boolean nova_lletra(char nova){
+		 boolean trobada = false;
+	        String paraula_old = paraula_usuari;
+	        paraula_usuari = "";
+	        for (int i=0; i<paraules[index].length(); i++) {
+	            if (paraules[index].charAt(i) == nova) {
+	                paraula_usuari += paraules[index].charAt(i);
+	                trobada = true;
+	            }
+	            else {
+	                paraula_usuari += paraula_old.charAt(i);
+	            }
+	        }
+	        return trobada;
+	    
+	}
+		
+}
+	
